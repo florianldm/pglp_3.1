096683cd1fa6693a996505cd2d893d808960217a
= <ledeme.florian@gmail.com>
2020-02-26 17:05:14 +0100

    Ajout sous classe qui s'occupe du calcul de salaire

---
+++ pglp_3.1/src/main/java/florianldm/CalculEmploye.java
@@ -0,0 +1,13 @@
+package florianldm;
+
+public class CalculEmploye extends Employe {
+
+    public CalculEmploye(String nom, String adresse) {
+        super(nom, adresse);
+    }
+
+    public double calculSalaire(){
+        //Calcul salaire//
+        return 0.;
+    }
+}
